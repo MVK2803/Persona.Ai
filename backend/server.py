ef8b87b14f0df850c79a8832e86b97b245f22326
MVK2803 <mathewvinukariath@gmail.com>
2023-02-11 18:40:48 +0530

    Backend and integration complete

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,23 @@
-from flask import Flask
+from flask import Flask,jsonify
+import requests
+import base64
 import api as persona
 from flask_cors import CORS
+import urllib.request
+from PIL import Image
+import json
 app = Flask(__name__)  #flask object
 CORS(app)
 # Pass the required route to the decorator.
 @app.route("/get_info",methods=['GET'])
 def hello():
     x=persona.generator()
-    
-    return x
+    text = {'message': x[0]}
+    image_url = x[1]
+    image_response = requests.get(image_url)
+    image_data = image_response.content
+    image = base64.b64encode(image_data).decode('utf-8')
+    response = {'text': text, 'image': image}
+    return jsonify(response)
 if __name__ == "__main__":
     app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,42 +3,36 @@ import axios from "axios";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
-  const [details, setDetails] = useState([]);
   const [img,setImg]=useState();
 
   const [image, setImage] = useState('');
   const handleSubmit = async (event) => {
     event.preventDefault();
     try{
-    const response = await axios.get(`http://127.0.0.1:5000/get_info`);
-    //alert(response.data[0]["Age"]);
-    
-    setInputValue(JSON.stringify(response.data[0]))
-    setImg(JSON.stringify(response.data[1]))
-    setImage("https://media.licdn.com/dms/image/D5603AQEYOiwH1AUbGw/profile-displayphoto-shrink_400_400/0/1670810985550?e=1681344000&v=beta&t=oIm3e4IsyXNAuwza99m2vpFgPFhnLxe7PDrVofLGSIk");
-    console.log(typeof(JSON.stringify(response.data[1])))}
+      //console.log("SUCESS");
+      const response = await axios.get(`http://127.0.0.1:5000/get_info`);
+      const image = new Blob([Uint8Array.from(atob(response.data.image), c => c.charCodeAt(0))], { type: 'image/png' });
+      setInputValue(JSON.stringify(response.data.text.message));
+      const imageUrl = URL.createObjectURL(image);
+      //console.log(imageUrl);
+      const imgElement = document.getElementById('img');
+      imgElement.src = imageUrl;
+    }
     catch(err)
     {
-      alert("TRY RELOADING .......")
+      alert("TRY RELOADING .......");
     }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          type="text"
-          value={inputValue}
-          
-        />
-        <button type="submit">Submit</button>
-        <textarea type="text" value={img}/>
-        <br />
-        <img src={image} alt="Generated Image" />
-        <br />
-      </form>
+    <>
       
-    </div>
+      <textarea type="text" value={inputValue}/>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+      <img  id ="img" src={img} alt="generated img"/>
+
+      
+    </>
   );
 }
 
