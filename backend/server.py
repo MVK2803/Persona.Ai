71ad5a7c4928becb50ac6cd20c4affddf291ddfd
MVK2803 <mathewvinukariath@gmail.com>
2023-12-20 20:12:24 +0530

    Bug Fixes

--- backend/server.py
+++ backend/server.py
@@ -7,7 +7,7 @@ CORS(app)
 # Pass the required route to the decorator.
 @app.route("/generate-persona",methods=['GET'])
 def hello():
-    print("hello")
+    print("API CALLED")
     return jsonify(genAPI.generatePersona())
 if __name__=="__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
